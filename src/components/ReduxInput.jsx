733a1f20658a65e8fe056c34a10ead167433c243
m-hatch <montehatch@gmail.com>
2018-06-14 21:59:11 -0400

    add input styles

--- src/components/ReduxInput.jsx
+++ src/components/ReduxInput.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 import createRenderer from './hoc/createRenderer'
 
 const ReduxInput = (input, meta) => 
-  <input { ...input } className={ meta.error && meta.touched ? 'error' : '' } />
+  <input { ...input } className={ (meta.error && meta.touched) ? 'form-control error' : 'form-control' } />
 
 export default createRenderer(ReduxInput)
--- src/components/ReduxSelect.jsx
+++ src/components/ReduxSelect.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import createRenderer from './hoc/createRenderer'
 
 const ReduxSelect = (input, meta, children) => 
-  <select { ...input } className={ meta.error && meta.touched ? 'error' : '' }>
+  <select { ...input } className={ (meta.error && meta.touched) ? 'form-control error' : 'form-control' }>
     { children }
   </select>
 
